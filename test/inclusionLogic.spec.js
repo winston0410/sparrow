3cd2ca51bea835ae6f8a9bec33c62652a2214be6
John Doe <hugosum@PC3343.local>
2020-08-12 18:31:03 +0800

    :white_check_mark: Update test for before operation

--- test/inclusionLogic.spec.js
+++ test/inclusionLogic.spec.js
@@ -118,8 +118,38 @@ describe('Test sparrow', function () {
   })
 
   describe('if operation is before', function () {
-    it('should insert a declaration before target declaration', function () {
+    it('should insert a declaration before target declaration', async function () {
+      const options = {
+        transformationList: [{
+          targets: ['$(a){font-size: $(a)}'], // css declaration with fill varible
+          transformationOption: [{
+            values: ['$(a){display: none}'],
+            operation: 'before' // append, prepend, insertBefore, insertAfter, replace
+          }]
+        }]
+      }
 
+      await postcss([
+        sparrow(options)
+      ])
+        .process(css, {
+          from: undefined
+        }).then(result => {
+          result.root.walkDecls((decl) => {
+            afterTransformation.push([decl.parent.selector, decl.prop, decl.value])
+          })
+        })
+
+      options.transformationList.forEach(({ targets, transformationOption }, index) => {
+        targets.forEach((target, index) => {
+          const targetDeclData = parseDecl(target)
+
+          if (isMatchingDecl(beforeTransformation[index], targetDeclData)) {
+            expect(isMatchingDecl(beforeTransformation[index], targetDeclData)).to.be.true
+            // Check if the length of afterTransformation array has increased
+          }
+        })
+      })
     })
   })
 
