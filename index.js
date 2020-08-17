1f3c4fdc803aaa24eb1b8ce860472ff1e98a34c1
John Doe <hugosum@PC3343.local>
2020-08-17 11:16:16 +0800

    :bug: Fix a bug where decl.parent.selector being unknown

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ module.exports = postcss.plugin('postcss-sparrow', ({
 
   return (root, result) => {
     root.walkDecls((decl) => {
+      const declDataList = listDeclData(decl)
+
       options.transformations.forEach((transformation) => {
         // Check if placeholder is missed.  Fill in if needed
-
-        const declDataList = listDeclData(decl)
         const targetDeclDataList = R.pipe(parseDecl, listDeclData)(transformation.target)
 
         // If two arrays match, run transformation
