9be0e3a69db8732b56cf58cb18c91f3dbd0bf2da
Winston <>
2020-09-30 11:06:13 +0800

    :fire: Remove unnecessary helper functions

--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -7,23 +7,6 @@ const isRegExp = isCorrectType(RegExp)
 const isBoolean = isCorrectType(Boolean)
 const isString = isCorrectType(String)
 
-const groupBySelector = R.groupBy(R.prop('selector'))
-
-// const mergeBySelector = R.map(R.reduce()())
-const concatValues = (k, l, r) => k === 'nodes' ? R.concat(l, r) : r
-
-const mergeNodes = R.reduce(R.mergeDeepWithKey(concatValues), R.head)
-
-const mergeNodesBySelector = R.pipe(
-  groupBySelector,
-  R.map(mergeNodes)
-)
-
-const mergeObjWithNewValues = (k, l, r) => R.when(
-  k === 'nodes',
-  R.always(r)
-)
-
 const shouldIncludeOrExclude = R.ifElse(
   R.propEq('inclusion', true)
 )
@@ -34,25 +17,11 @@ const ifHasWildCard = R.ifElse(
   hasWildCard
 )
 
-const fromNestedLoop = (fn) => R.map(
-  R.map(
-    fn
-  )
-)
-
-const ifOperationEqual = (operation) => R.pipe(
-  R.prop('operation'),
-  R.equals(operation)
-)
-
 module.exports = {
   isArray,
   isRegExp,
   isBoolean,
   isString,
-  mergeNodesBySelector,
   shouldIncludeOrExclude,
-  ifHasWildCard,
-  fromNestedLoop,
-  ifOperationEqual
+  ifHasWildCard
 }
